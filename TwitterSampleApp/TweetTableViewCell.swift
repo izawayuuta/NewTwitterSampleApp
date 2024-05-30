5c9a46d52908b910029ab94e60037c831b109052
iosアカデミア <yutamac2512@icloud.com>
2024-05-30 20:31:54 +0900

    .gitignoreファイルを新規登録

---
+++ TwitterSampleApp/TweetTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TweetTableViewCell.swift
+//  TwitterSampleApp
+//
+//  Created by 俺の MacBook Air on 2024/05/30.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
